9a28e86381d9fda9cb1ab4b92e5ae3f306148a46
shaikhaftab47933-hue <shaikhaftab47933@gmail.com>
2026-06-03 13:56:27 +0530

    Update App.js configuration for new settings

--- App.js
+++ App.js
@@ -6,26 +6,32 @@
     "orientation": "portrait",
     "icon": "./assets/icon.png",
     "userInterfaceStyle": "light",
+    "newArchEnabled": true,
     "splash": {
       "image": "./assets/splash.png",
       "resizeMode": "contain",
-      "backgroundColor": "#0f0c29"
+      "backgroundColor": "#ffffff"
     },
-    "assetBundlePatterns": [
-      "**/*"
-    ],
     "ios": {
       "supportsTablet": true
     },
     "android": {
+      "package": "com.aftab.creatorspulse",
+      "edgeToEdgeEnabled": true,
       "adaptiveIcon": {
         "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#0f0c29"
-      },
-      "package": "com.creatorspulse.app"
+        "backgroundColor": "#ffffff"
+      }
     },
-    "web": {
-      "favicon": "./assets/favicon.png"
-    }
+    "extra": {
+      "eas": {
+        "projectId": "c8c7236e-f81c-43ad-8db0-8887820fc598"
+      }
+    },
+    "sdkVersion": "54.0.0",
+    "platforms": [
+      "ios",
+      "android"
+    ]
   }
 }
