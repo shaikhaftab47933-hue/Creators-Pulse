a18c7e4abb57cbac4495b66bc5ed70b1ec5575c4
shaikhaftab47933-hue <shaikhaftab47933@gmail.com>
2026-06-03 15:25:53 +0530

    Update app slug and package name in App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 {
   "expo": {
     "name": "Creators Pulse",
-    "slug": "creators-pulse-new",
+    "slug": "creators-pulse",
     "version": "1.0.0",
     "orientation": "portrait",
     "icon": "./assets/icon.png",
@@ -16,11 +16,16 @@
       "supportsTablet": true
     },
     "android": {
-      "package": "com.creatorspulse.app",
+      "package": "com.aftab.creatorspulse",
       "edgeToEdgeEnabled": true,
       "adaptiveIcon": {
         "foregroundImage": "./assets/adaptive-icon.png",
         "backgroundColor": "#ffffff"
+      }
+    },
+    "extra": {
+      "eas": {
+        "projectId": "c8c7236e-f81c-43ad-8db0-8887820fc598"
       }
     },
     "sdkVersion": "54.0.0",
