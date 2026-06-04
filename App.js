51128cbe087dbc10628a8d956401c227d6a839e1
shaikhaftab47933-hue <shaikhaftab47933@gmail.com>
2026-06-04 10:32:39 +0530

    Refactor App.js configuration settings
    Removed newArchEnabled and extra fields, updated backgroundColor and package name.

--- App.js
+++ App.js
@@ -6,33 +6,28 @@
     "orientation": "portrait",
     "icon": "./assets/icon.png",
     "userInterfaceStyle": "light",
-    "newArchEnabled": true,
     "splash": {
       "image": "./assets/splash.png",
       "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+      "backgroundColor": "#0f0c29"
     },
+    "assetBundlePatterns": [
+      "**/*"
+    ],
     "ios": {
       "supportsTablet": true
     },
     "android": {
-      "package": "com.aftab.creatorspulse",
-      "edgeToEdgeEnabled": true,
       "adaptiveIcon": {
         "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
-      }
+        "backgroundColor": "#0f0c29"
+      },
+      "package": "com.creatorspulse.app"
     },
-    "extra": {
-      "eas": {
-        "projectId": "4cae8cbf-1b1c-4ef4-9fc7-baa6fc3f60d7"
-      }
-    },
-    "sdkVersion": "54.0.0",
-    "platforms": [
-      "ios",
-      "android"
-    ]
+    "web": {
+      "favicon": "./assets/favicon.png"
+    }
   }
 }
+
 
